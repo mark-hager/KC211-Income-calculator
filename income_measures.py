91609c905723ab9a24d459fbc8ac645e89458c97
markhgr <hagerm@oregonstate.edu>
2024-01-17 17:03:58 -0500

    updated FPL to 2024

--- income_measures.py
+++ income_measures.py
@@ -18,13 +18,13 @@ def excel_ceil(num):
 
 def calculate_fpl(client):
     """
-    Calculates the Federal Poverty Level based on HHS 2023 guidelines
+    Calculates the Federal Poverty Level based on HHS 2024 guidelines
     https://aspe.hhs.gov/topics/poverty-economic-mobility/poverty-guidelines
     """
 
     # FPL is calculated with a base rate times an additional rate per person
-    fpl_base = 9440
-    fpl_rate_per_person = 5140
+    fpl_base = 9680
+    fpl_rate_per_person = 5380
     # calculate the FPL by dividing income by the base rate + household size
     # * the rate per person
     fpl = math.ceil(client.annual_income / ((client.household_size * fpl_rate_per_person)
